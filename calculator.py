f3d871e14d5eed435e6d74e615499032c5dea919
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-06 11:19:09 +0600

    calcualtor python course

---
+++ calculator.py
@@ -0,0 +1,21 @@
+
+def add(first, second):
+    # return должен вернуть ответ функции, в данном случае first + second
+    
+    return first + second
+
+def sub(first, second):
+    # return subtraction of first and second arguments
+	return first - second
+
+def multiply(first, second):
+    # return multiplicatiom of first and second arguments
+	return first * second
+
+def div_with_floor(first, second):
+    # return division with floor of first and second arguments
+    return first // second
+
+def div_without_floor(first, second):
+    # return division without floor of first and second arguments
+	return first / second
